81408713be891b88aef77c3fa643b14320fec2e0
Anna Viklund <annamariaviklund@gmail.com>
2024-10-28 12:10:39 +0100

    Setup component Select

---
+++ ui-kit/src/components/Select/Select.stories.tsx
@@ -0,0 +1,34 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import {
+  Select,
+  SelectContent,
+  SelectItem,
+  SelectTrigger,
+  SelectValue,
+} from "./Select";
+
+const meta: Meta<typeof Select> = {
+  component: Select,
+  render: (args) => (
+    <Select {...args}>
+      <SelectTrigger className="w-64">
+        <SelectValue placeholder="Select a fruit" />
+      </SelectTrigger>
+      <SelectContent>
+        <SelectItem value="apple">Apple</SelectItem>
+        <SelectItem value="banana">Banana</SelectItem>
+        <SelectItem value="blueberry">Blueberry</SelectItem>
+        <SelectItem value="grapes">Grapes</SelectItem>
+        <SelectItem value="pineapple">Pineapple</SelectItem>
+      </SelectContent>
+    </Select>
+  ),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Select>;
+
+export const Default: Story = {};
+
+export const Disabled: Story = { args: { disabled: true } };
