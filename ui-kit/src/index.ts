9e64c210256394846bb558c20aaf21e81571ae63
Anna Viklund <annamariaviklund@gmail.com>
2024-10-30 11:04:27 +0100

    Add lib entry point

---
+++ ui-kit/src/index.ts
@@ -0,0 +1,4 @@
+export { Box } from "./components/Box/Box";
+export { Input } from "./components/Input/Input";
+export { Select } from "./components/Select/Select";
+export { Slider } from "./components/Slider/Slider";
