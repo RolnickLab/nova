4b1948195897789f3771458a75954809ba03fbbb
Anna Viklund <annamariaviklund@gmail.com>
2024-10-30 11:22:11 +0100

    Update build config

--- ui-kit/vite.config.ts
+++ ui-kit/vite.config.ts
@@ -5,6 +5,21 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    lib: {
+      entry: "./src/index.ts",
+      name: "nova-ui-kit",
+      fileName: (format) => `nova-ui-kit.${format}.js`,
+    },
+    rollupOptions: {
+      external: ["react", "react-dom"],
+      output: {
+        globals: {
+          react: "React",
+        },
+      },
+    },
+  },
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
