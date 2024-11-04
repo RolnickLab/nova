bfeb47b3e25bd2abe1173499cb2edb4d4e7c0992
Anna Viklund <annamariaviklund@gmail.com>
2024-11-04 22:01:36 +0100

    Tweak box margin

--- ui-kit/src/components/Box/Box.tsx
+++ ui-kit/src/components/Box/Box.tsx
@@ -10,7 +10,7 @@ interface BoxProps {
 export const Box = ({ className, label, children }: BoxProps) => (
   <div className={cn("p-4 border border-border rounded-xl", className)}>
     {label?.length ? (
-      <div className="h-8 flex items-center justify-between">
+      <div className="h-8 flex items-center justify-between mx-2">
         <span className="body-overline font-bold">{label}</span>
       </div>
     ) : null}
