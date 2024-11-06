2d0c9af757cb99bec8930527215889904d3da8a0
Anna Viklund <annamariaviklund@gmail.com>
2024-11-06 14:34:11 +0100

    Setup component Button

---
+++ ui-kit/src/components/Button/Button.stories.tsx
@@ -0,0 +1,82 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { ChevronRight, Mail } from "lucide-react";
+import { Button } from "./Button";
+
+const meta: Meta<typeof Button> = {
+  component: Button,
+  argTypes: {
+    asChild: {
+      table: {
+        disable: true,
+      },
+    },
+    size: {
+      options: ["default", "icon"],
+      control: { type: "radio" },
+    },
+    variant: {
+      options: ["default", "destructive", "outline", "ghost", "link", "icon"],
+      control: { type: "radio" },
+    },
+  },
+  render: (args) => (
+    <Button {...args}>
+      <span>Button</span>
+    </Button>
+  ),
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Button>;
+
+export const Default: Story = {
+  args: {
+    variant: "default",
+    disabled: false,
+    size: "default",
+  },
+};
+
+export const Secondary: Story = {
+  args: { ...Default.args, variant: "secondary" },
+};
+
+export const Destructive: Story = {
+  args: { ...Default.args, variant: "destructive" },
+};
+
+export const Outline: Story = {
+  args: { ...Default.args, variant: "outline" },
+};
+
+export const Ghost: Story = {
+  args: { ...Default.args, variant: "ghost" },
+};
+
+export const Link: Story = {
+  args: { ...Default.args, variant: "link" },
+};
+
+export const Icon: Story = {
+  args: { ...Default.args, size: "icon" },
+  render: (args) => (
+    <Button {...args}>
+      <ChevronRight className="w-4 h-4 " />
+    </Button>
+  ),
+};
+
+export const WithIcon: Story = {
+  args: { ...Default.args },
+  render: (args) => (
+    <Button {...args}>
+      <Mail className="w-4 h-4 mr-2" />
+      <span>Login with Email</span>
+    </Button>
+  ),
+};
+
+export const Disabled: Story = {
+  args: { ...Default.args, disabled: true },
+};
