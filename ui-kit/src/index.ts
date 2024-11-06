477257643fbfe5814ce1faa3fa67216aa71fac73
Anna Viklund <annamariaviklund@gmail.com>
2024-11-06 14:36:56 +0100

    Update NPM package with new components

--- ui-kit/src/index.ts
+++ ui-kit/src/index.ts
@@ -3,6 +3,11 @@ import "./typography.css";
 export { CONSTANTS } from "./lib/constants";
 
 export { Box } from "./components/Box/Box";
+export { Button } from "./components/Button/Button";
+export { buttonVariants } from "./components/Button/buttonVariants";
+export * as Combobox from "./components/Combobox/Combobox";
+export * as Command from "./components/Command/Command";
 export { Input } from "./components/Input/Input";
+export * as Popover from "./components/Popover/Popover";
 export * as Select from "./components/Select/Select";
 export { Slider } from "./components/Slider/Slider";
