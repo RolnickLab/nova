0db5a2112b1f0c705a7ca2368cedf5cfa56a90ab
Anna Viklund <annamariaviklund@gmail.com>
2024-11-11 20:51:48 +0100

    Add example data constants

---
+++ ui-kit/src/stories/constants.ts
@@ -0,0 +1,45 @@
+export const PLACEHOLDER_LABEL = "Catocala relicta luna";
+
+export const PLACEHOLDER_TITLE =
+  "Catocala relicta luna argyroploce morrisoni pygarctia";
+
+export const PLACEHOLDER_TEXT =
+  "Catocala relicta luna argyroploce morrisoni pygarctia. Zygaena eupithecia noctuidae hyalophora eriogonum purissima lithophane.";
+
+export const EXAMPLE_TAXA = [
+  {
+    value: "limacodinae",
+    label: "Limacodinae",
+    rank: "Subfamily",
+    level: 0,
+    hasChildren: true,
+  },
+  {
+    value: "acharia",
+    label: "Acharia",
+    rank: "Genus",
+    level: 1,
+    hasChildren: true,
+  },
+  {
+    value: "acharia-stimule",
+    label: "Acharia stimulea",
+    rank: "Species",
+    level: 2,
+    hasChildren: false,
+  },
+  {
+    value: "achatia-distincta",
+    label: "Achatia distincta",
+    rank: "Unknown",
+    level: 0,
+    hasChildren: false,
+  },
+  {
+    value: "acleris-braunana",
+    label: "Acleris braunana",
+    rank: "Species",
+    level: 0,
+    hasChildren: false,
+  },
+];
