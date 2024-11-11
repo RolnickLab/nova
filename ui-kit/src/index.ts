a67a2863da9a171e011d5de125a44a894ea3e832
Anna Viklund <annamariaviklund@gmail.com>
2024-11-11 21:21:05 +0100

    Setup component Collapsible

--- ui-kit/src/index.ts
+++ ui-kit/src/index.ts
@@ -5,6 +5,7 @@ export { CONSTANTS } from "./lib/constants";
 export { Box } from "./components/Box/Box";
 export { Button } from "./components/Button/Button";
 export { buttonVariants } from "./components/Button/buttonVariants";
+export * as Collapsible from "./components/Collapsible/Collapsible";
 export * as Command from "./components/Command/Command";
 export { Input } from "./components/Input/Input";
 export * as Popover from "./components/Popover/Popover";
