57be689da8b362b30cc09e88e89ad30d3e019d1e
Anna Viklund <annamariaviklund@gmail.com>
2024-11-12 17:43:53 +0100

    Setup component Calendar

---
+++ ui-kit/src/components/Calendar/Calendar.stories.tsx
@@ -0,0 +1,20 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Calendar } from "./Calendar";
+
+const meta: Meta<typeof Calendar> = {
+  component: Calendar,
+};
+
+export default meta;
+
+type Story = StoryObj<typeof Calendar>;
+
+export const Default: Story = {
+  args: {},
+};
+
+export const WithSelectedDate: Story = {
+  args: {
+    selected: new Date(),
+  },
+};
