f3b00d377072c34e23927347be619ab4d7e95f08
Anna Viklund <annamariaviklund@gmail.com>
2024-11-13 10:29:45 +0100

    Update Calendar styles and add more examples to documentation

--- ui-kit/src/components/Calendar/Calendar.stories.tsx
+++ ui-kit/src/components/Calendar/Calendar.stories.tsx
@@ -1,8 +1,15 @@
 import type { Meta, StoryObj } from "@storybook/react";
+import { addDays } from "date-fns";
 import { Calendar } from "./Calendar";
 
 const meta: Meta<typeof Calendar> = {
   component: Calendar,
+  argTypes: {
+    mode: {
+      options: ["default", "single", "multiple", "range"],
+      control: { type: "radio" },
+    },
+  },
 };
 
 export default meta;
@@ -10,11 +17,34 @@ export default meta;
 type Story = StoryObj<typeof Calendar>;
 
 export const Default: Story = {
-  args: {},
+  args: {
+    mode: "default",
+  },
 };
 
 export const WithSelectedDate: Story = {
   args: {
+    ...Default.args,
+    mode: "single",
     selected: new Date(),
+  },
+};
+
+export const WithSelectedDates: Story = {
+  args: {
+    ...Default.args,
+    mode: "multiple",
+    selected: [addDays(new Date(), -2), new Date(), addDays(new Date(), 2)],
+  },
+};
+
+export const WithSelectedDateRange: Story = {
+  args: {
+    ...Default.args,
+    mode: "range",
+    selected: {
+      from: addDays(new Date(), -2),
+      to: addDays(new Date(), 2),
+    },
   },
 };
--- ui-kit/src/components/Calendar/Calendar.tsx
+++ ui-kit/src/components/Calendar/Calendar.tsx
@@ -27,12 +27,13 @@ function Calendar({ className, classNames, ...props }: CalendarProps) {
         head_cell:
           "text-muted-foreground rounded-md w-9 font-normal text-[0.8rem]",
         row: "flex w-full mt-2",
-        cell: "h-9 w-9 text-center body-small p-0 relative [&:has([aria-selected].day-range-end)]:rounded-r-md [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected])]:bg-accent first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
+        cell: "h-9 w-9 text-center body-small p-0 relative [&:has([aria-selected].day-range)]:bg-accent [&:has([aria-selected].day-outside)]:bg-accent/50 [&:has([aria-selected].day-range-start)]:rounded-l-md [&:has([aria-selected].day-range-end)]:rounded-r-md first:[&:has([aria-selected])]:rounded-l-md last:[&:has([aria-selected])]:rounded-r-md focus-within:relative focus-within:z-20",
         day: cn(
           buttonVariants({ variant: "ghost" }),
           "body-number h-9 w-9 p-0 pt-0.5 font-normal aria-selected:opacity-100"
         ),
-        day_range_end: "day-range-end",
+        day_range_start: "day-range day-range-start",
+        day_range_end: "day-range day-range-end",
         day_selected:
           "bg-primary text-primary-foreground hover:bg-primary hover:text-primary-foreground focus:bg-primary focus:text-primary-foreground",
         day_today: cn(buttonVariants({ variant: "outline" })),
@@ -40,7 +41,7 @@ function Calendar({ className, classNames, ...props }: CalendarProps) {
           "day-outside text-muted-foreground aria-selected:bg-accent/50 aria-selected:text-muted-foreground",
         day_disabled: "text-muted-foreground opacity-50",
         day_range_middle:
-          "aria-selected:bg-accent aria-selected:text-accent-foreground",
+          "day-range aria-selected:bg-accent aria-selected:text-accent-foreground",
         day_hidden: "invisible",
         ...classNames,
       }}
