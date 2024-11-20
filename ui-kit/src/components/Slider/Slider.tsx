cd6f6fb23e764a322959d08141de2343db1233e9
Anna Viklund <annamariaviklund@gmail.com>
2024-11-20 11:58:16 +0100

    Revert to primary color

--- ui-kit/src/components/Slider/Slider.tsx
+++ ui-kit/src/components/Slider/Slider.tsx
@@ -19,16 +19,16 @@ const Slider = React.forwardRef<
     <SliderPrimitive.Track
       className={cn(
         "relative h-2 w-full grow overflow-hidden rounded-full bg-border",
-        { "bg-success-500": invertedColors }
+        { "bg-primary": invertedColors }
       )}
     >
       <SliderPrimitive.Range
-        className={cn("absolute h-full bg-success-500", {
+        className={cn("absolute h-full bg-primary", {
           "bg-border": invertedColors,
         })}
       />
     </SliderPrimitive.Track>
-    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-success-600 bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
+    <SliderPrimitive.Thumb className="block h-5 w-5 rounded-full border-2 border-primary bg-background ring-offset-background transition-colors focus-visible:outline-none focus-visible:ring-2 focus-visible:ring-ring focus-visible:ring-offset-2 disabled:pointer-events-none disabled:opacity-50" />
   </SliderPrimitive.Root>
 ));
 Slider.displayName = "Slider";
