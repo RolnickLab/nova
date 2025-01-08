73e6aea92044771654bcc33cf5ea58f387eaae80
Anna Viklund <annamariaviklund@gmail.com>
2025-01-08 15:03:10 +0100

    Update export

--- ui-kit/src/index.ts
+++ ui-kit/src/index.ts
@@ -8,6 +8,7 @@ export { buttonVariants } from "./components/Button/buttonVariants";
 export { Calendar } from "./components/Calendar/Calendar";
 export * as Collapsible from "./components/Collapsible/Collapsible";
 export * as Command from "./components/Command/Command";
+export { IdentificationStatus } from "./components/IdentificationStatus/IdentificationStatus";
 export { Input } from "./components/Input/Input";
 export * as Popover from "./components/Popover/Popover";
 export * as Select from "./components/Select/Select";
