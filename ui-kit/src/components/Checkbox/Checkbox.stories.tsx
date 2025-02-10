4642aeaad8771834778ee3e1bda15e6c26a782b4
Anna Viklund <annamariaviklund@gmail.com>
2025-02-10 14:13:52 +0100

    Setup checkbox component

---
+++ ui-kit/src/components/Checkbox/Checkbox.stories.tsx
@@ -0,0 +1,35 @@
+import type { Meta, StoryObj } from "@storybook/react";
+import { Checkbox } from "./checkbox";
+
+const meta: Meta<typeof Checkbox> = {
+  component: Checkbox,
+  render: (args) => (
+    <div className="flex items-center gap-2">
+      <Checkbox {...args} />
+      <label
+        htmlFor={args.id}
+        className="pt-0.5 body-small text-muted-foreground peer-disabled:cursor-not-allowed peer-disabled:opacity-50"
+      >
+        Accept terms and conditions
+      </label>
+    </div>
+  ),
+} satisfies Meta<typeof Checkbox>;
+
+export default meta;
+
+type Story = StoryObj<typeof meta>;
+
+export const Default: Story = {
+  args: {
+    id: "terms",
+    disabled: false,
+  },
+};
+
+export const Disabled: Story = {
+  args: {
+    ...Default.args,
+    disabled: true,
+  },
+};
